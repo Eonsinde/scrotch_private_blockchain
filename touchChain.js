9bcfb04fc8e7bd085cae79a1133231a725a1b316
eonsinde <olasinde.eon@gmail.com>
2022-12-13 14:55:56 +0100

    exported Blockchain and Block classes

--- touchChain.js
+++ touchChain.js
@@ -38,17 +38,8 @@ class Blockchain {
     }
 }
 
-// creating blocks
-const testBlock = new Block([])
-const testBlock2 = new Block([])
-const testBlock3 = new Block([])
-const testBlock4 = new Block([])
 
-// create blockchain instance
-const testBlockchain = new Blockchain()
-testBlockchain.addBlock(testBlock)
-testBlockchain.addBlock(testBlock2)
-testBlockchain.addBlock(testBlock3)
-testBlockchain.addBlock(testBlock4)
-
-console.log(testBlockchain)
+module.exports = {
+    Block,
+    Blockchain
+}
